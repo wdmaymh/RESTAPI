e6f5b7e1213c23a0ab8709ce82ad1844386ee693
wdmaymh <wdmaymh@naver.com>
2022-06-29 11:13:46 +0900

    real/test DB properties & p6spy config

--- src/test/java/ac/korea/isdevelop/restapi/billMaster/BillMasterControllerTest.java
+++ src/test/java/ac/korea/isdevelop/restapi/billMaster/BillMasterControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class BillMasterControllerTest {
 
     @Autowired
@@ -103,7 +105,7 @@ class BillMasterControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("billNo").exists())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE+";charset=UTF-8"))
                 .andExpect(jsonPath("billNo").value(Matchers.not(100)))
 
         ;
